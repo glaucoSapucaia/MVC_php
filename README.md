cc005203d6281ba543bd2a5a039a42e666b8d261
glaucoSapucaia <glaucopco@gmail.com>
2024-07-06 13:41:32 -0300

    Composer install and config

--- README.md
+++ README.md
@@ -3,3 +3,17 @@ Model View Controler com PHP
 
 ## Iniciando Local Server PHP
 ![alt text](imgs/assets/image.png)
+
+## Composer
+### [Composer DOC](https://getcomposer.org/)  
+- Install  
+![alt text](imgs/assets/image-1.png)  
+- Verificando integridade  
+![alt text](imgs/assets/image-2.png)  
+- Executando script
+![alt text](imgs/assets/image-3.png)  
+- Removendo script após instalação  
+![alt text](imgs/assets/image-4.png)  
+- Create composer.json file  
+- Run .phar file  
+![alt text](imgs/assets/image-5.png)
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'glauco-sapucaia/mvc_php',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'cdf11185db51d1709dedc516bdc46385a57a06c4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'glauco-sapucaia/mvc_php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'cdf11185db51d1709dedc516bdc46385a57a06c4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
